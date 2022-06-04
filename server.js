568ae13067ae1d19e4faf62dae1c14d224a45b41
Agung Adityatama <agung.adit121@gmail.com>
2022-06-05 02:11:53 +0700

    setting routes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 //parser application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//panggil routes
+var routes routes(app: any): void
+routes(app);
+
 app.listen(1234, () => {
     console.log('Server started on port');
 });
